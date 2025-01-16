c7ddd6ae1ec726303f2cb614801f535e7611b8fc
Patryck Silva <patrycksilva70@gmail.com>
2025-01-16 15:19:34 -0300

    refactor: update PersonalProjectsCard layout for improved responsiveness

--- src/components/Frames/ProjectsFrame/PersonalProjects/index.tsx
+++ src/components/Frames/ProjectsFrame/PersonalProjects/index.tsx
@@ -61,7 +61,7 @@ const PersonalProjectsCard = ({
       <figure
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
-        className={`relative aspect-square h-[350px] xs:h-[400px]`}
+        className={`relative aspect-square h-full min-h-[350px] w-full lg:min-h-[400px]`}
       >
         {isHovered && (
           <div className={`relative size-full`}>
