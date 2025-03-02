e6fab9461feaa3c881cf2106ed966efd2961b6fb
Patryck Silva <patrycksilva70@gmail.com>
2025-03-02 14:18:13 -0300

    feat: add CV download button and refactor social media buttons layout

--- src/components/ui/social-media-buttons.tsx
+++ src/components/ui/social-media-buttons.tsx
@@ -6,40 +6,50 @@ import {
 import { cn } from "@/lib/utils";
 import { links } from "@/utils/constants";
 import Link from "next/link";
+import { TbFileCv } from "react-icons/tb";
+import { Button } from "./button";
 
 export const SocialMediasButtons = () => {
   const socialMedias = [
     {
       name: "LinkedIn",
       link: links.linkedin,
-      icon: <DarkLinkedinLogo />,
+      icon: DarkLinkedinLogo,
     },
     {
       name: "GitHub",
       link: links.github,
-      icon: <DarkGithubLogo />,
+      icon: DarkGithubLogo,
     },
     {
       name: "Gmail",
       link: "/#contact",
-      icon: <DarkGmailLogo />,
+      icon: DarkGmailLogo,
     },
   ];
 
   return (
-    <aside className={`flex items-end gap-3`}>
-      {socialMedias.map((item, index) => (
-        <Link
-          key={item.name}
-          href={item.link}
-          target={index === 2 ? "_self" : "_blank"}
-          className={cn(
-            `h-max w-8 text-foreground/70 transition-transform duration-300 ease-in-out hover:scale-110 hover:text-foreground`,
-          )}
-        >
-          {item.icon}
-        </Link>
-      ))}
-    </aside>
+    <section className={`flex flex-col gap-3.5`}>
+      <aside className={`flex items-end gap-3`}>
+        {socialMedias.map((item, index) => (
+          <Link
+            key={item.name}
+            href={item.link}
+            target={index === 2 ? "_self" : "_blank"}
+            className={cn(
+              `h-max w-8 text-foreground/70 transition-transform duration-300 ease-in-out hover:scale-110 hover:text-foreground`,
+            )}
+          >
+            <item.icon />
+          </Link>
+        ))}
+      </aside>
+      <a
+        href={"/pdfs/patryck_silva_cv_fullstack_pt.pdf"}
+        download={"Patryck_Silva_CV"}
+      >
+        <Button variant={"outline"}>Baixar CV</Button>
+      </a>
+    </section>
   );
 };
